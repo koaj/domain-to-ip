05039b27572015007b98c702c58eec95bfffc1eb
Name <you@example.com>
2021-03-25 13:49:12 +0400

    Add TLS resolvers into CLI and remove error message in default mode.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::io;
 use std::io::BufRead;
 use std::thread;
 use std::time::Duration;
+use std::{env::args, io};
+use trust_dns_resolver::config::*;
 use trust_dns_resolver::Resolver;
 
 fn main() {
@@ -14,8 +15,16 @@ fn host_to_ip() {
     for line in input.lock().lines() {
         domains.push(line.unwrap());
     }
-
-    let resolver = Resolver::from_system_conf().unwrap();
+    // Capturing all arguments.
+    let all_args: Vec<String> = args().collect();
+    // Check for configuration in args.
+    // On Unix systems by this app use /etc/resolv.conf as resolver.
+    let mut resolver = Resolver::from_system_conf().unwrap();
+    if all_args.contains(&"secure".to_string()) {
+        // Using cloudflare 1.1.1.1 DNS service if there were `secure` keyword in input args.
+        resolver =
+            Resolver::new(ResolverConfig::cloudflare_tls(), ResolverOpts::default()).unwrap();
+    }
     let handle = thread::spawn(move || {
         for dom in &domains {
             if !dom.is_empty() {
@@ -26,10 +35,13 @@ fn host_to_ip() {
                     .map(|lookup_ip| lookup_ip.iter().next().map(|ip| println!("{}|{}", dom, ip)))
                     .is_err()
                 {
-                    eprintln!("{}|Nothing found", dom);
+                    // Checking input args for the `debug` keyword to print error.
+                    if all_args.contains(&"debug".to_string()) {
+                        eprintln!("{}|Nothing found", dom);
+                    }
                 }
 
-                thread::sleep(Duration::from_millis(8));
+                thread::sleep(Duration::from_millis(1));
             }
         }
     });
